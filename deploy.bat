3c3c4683f3fd3af79a2370f1588227c945cbeaf0
crusoe@gmx.net <Bivvo0-hirmax-wifrym>
2021-02-07 15:11:23 +0100

    Initial commit

---
+++ deploy.bat
@@ -0,0 +1,2 @@
+heroku war:deploy target\bingo-mk-1.0.war --app bingo-mk
+pause
---
+++ node_modules/@vaadin/flow-deps/generated-flow-imports.js
@@ -0,0 +1,37 @@
+const div = document.createElement('div');
+div.innerHTML = '<custom-style><style include="lumo-color lumo-typography"></style></custom-style>';
+document.head.insertBefore(div.firstElementChild, document.head.firstChild);
+
+import '@vaadin/flow-frontend/flow-component-renderer.js';
+import '@vaadin/vaadin-button/theme/lumo/vaadin-button.js';
+import '@vaadin/vaadin-dialog/theme/lumo/vaadin-dialog.js';
+import '@vaadin/vaadin-icons/vaadin-icons.js';
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/icons.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-notification/theme/lumo/vaadin-notification.js';
+import '@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/theme/lumo/vaadin-text-field.js';
+var scripts = document.getElementsByTagName('script');
+var thisScript;
+var elements = document.getElementsByTagName('script');
+for (var i = 0; i < elements.length; i++) {
+    var script = elements[i];
+    if (script.getAttribute('type')=='module' && script.getAttribute('data-app-id') && !script['vaadin-bundle']) {
+        thisScript = script;break;
+     }
+}
+if (!thisScript) {
+    throw new Error('Could not find the bundle script to identify the application id');
+}
+thisScript['vaadin-bundle'] = true;
+if (!window.Vaadin.Flow.fallbacks) { window.Vaadin.Flow.fallbacks={}; }
+var fallbacks = window.Vaadin.Flow.fallbacks;
+fallbacks[thisScript.getAttribute('data-app-id')] = {}
+fallbacks[thisScript.getAttribute('data-app-id')].loadFallback = function loadFallback(){
+   return import('./generated-flow-imports-fallback.js');
+}
